f80d08c0d8f2f93238c7a7a2e981bee86dabd8fa
Sidratul Muntaha Sara <sidratul.s@outlook.com>
2024-07-28 11:24:01 +0400

    updated

---
+++ BetaHub Website/main.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        alert("Success!");
+      })
+    });
+  });
+  
