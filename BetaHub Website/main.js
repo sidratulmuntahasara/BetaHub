6236465cf95023ebfa995196257378ab542929d8
Sidratul Muntaha Sara <sidratul.s@outlook.com>
2024-07-29 15:07:46 +0400

    updated

--- BetaHub Website/main.js
+++ BetaHub Website/main.js
@@ -4,6 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -16,12 +17,11 @@ const firebaseConfig = {
   measurementId: "G-VK01KYTFPJ"
 };
 
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-
-
 
 window.addEventListener("load", function() {
     const form = document.getElementById('my-form');
