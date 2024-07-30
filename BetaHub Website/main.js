88e34b58cb5bcaa7e2a4d7fa1f14082f33966edc
Sidratul Muntaha Sara <sidratul.s@outlook.com>
2024-07-30 05:56:11 +0400

    updated

--- BetaHub Website/main.js
+++ BetaHub Website/main.js
@@ -1,27 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDHeCLdjTmA8Y5q8f_OullnWS4CorIEs-A",
-  authDomain: "betahub-ai.firebaseapp.com",
-  projectId: "betahub-ai",
-  storageBucket: "betahub-ai.appspot.com",
-  messagingSenderId: "226120833718",
-  appId: "1:226120833718:web:4c5bea87f908cb52f8e417",
-  measurementId: "G-VK01KYTFPJ"
-};
-
-
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 window.addEventListener("load", function() {
     const form = document.getElementById('my-form');
