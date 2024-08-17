518c9f7c69341071ece4ac8fde1c03ee57a4553c
Sidratul Muntaha Sara <sidratul.s@outlook.com>
2024-08-17 22:44:05 +0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Main from "./main/page";
 import { Box } from "@mui/material";
+// import Scroll from '@/components/scroll'
 
 export default function Home() {
   return (
     <Box height={'100vh'} width={'100vw'} className='bg-black overflow-x-hidden -z-50'>
-        
+        {/* <Scroll /> */}
       <Main/> 
     </Box>
   );
